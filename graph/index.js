34b11085589c98a1d62b6ddb3d8725c7bacf242b
SamDeiter <sam.deiter@epicgames.com>
2025-11-23 21:32:35 -0500

    Fix export paths in graph/index.js barrel file

--- graph/index.js
+++ graph/index.js
@@ -3,7 +3,7 @@
  * This file manages the data model, rendering, and all user interactions
  * (pan, zoom, drag, selection, wiring).
  */
-export { Pin } from './graph/Pin.js';
-export { Node } from './graph/Node.js';
-export { WiringController } from './graph/WiringController.js';
-export { GraphController } from './graph/GraphController.js';
+export { Pin } from './Pin.js';
+export { Node } from './Node.js';
+export { WiringController } from './WiringController.js';
+export { GraphController } from './GraphController.js';
